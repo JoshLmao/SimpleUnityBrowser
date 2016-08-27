a7b6f4d929041341e1959774fd0771ba71cd9396
Wch <Wch@localhost>
2016-08-27 15:26:10 +0300

    Redirect popups

--- SharedPluginServer/CefWorker/WorkerCefLifespanHandler.cs
+++ SharedPluginServer/CefWorker/WorkerCefLifespanHandler.cs
@@ -19,7 +19,8 @@ namespace SharedPluginServer
             MainBrowser = browser;
             MainBrowserHost = browser.GetHost();
         }
-       protected override bool DoClose(CefBrowser browser)
+
+        protected override bool DoClose(CefBrowser browser)
         {
 
             // return false;
@@ -29,6 +30,17 @@ namespace SharedPluginServer
         protected override void OnBeforeClose(CefBrowser browser)
         {
             _mainWorker.BrowserMessageRouter.OnBeforeClose(browser);
+        }
+
+        protected override bool OnBeforePopup(CefBrowser browser, CefFrame frame, string targetUrl,
+            string targetFrameName, CefWindowOpenDisposition targetDisposition, bool userGesture,
+            CefPopupFeatures popupFeatures, CefWindowInfo windowInfo, ref CefClient client, CefBrowserSettings settings,
+            ref bool noJavascriptAccess)
+        {
+            // Block a new popup window.
+            // Instead just redirect the popup target to the current browser.
+            browser.GetMainFrame().LoadUrl(targetUrl);
+            return true;
         }
     }
 }
